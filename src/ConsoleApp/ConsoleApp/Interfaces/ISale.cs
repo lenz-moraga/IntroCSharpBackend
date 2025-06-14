828fb0fd9a8b8bdeb1942f1d8b11487e50f65e8f
lenz-moraga <49219425+lenz-moraga@users.noreply.github.com>
2025-06-13 21:29:44 -0600

    Add ISale and ISave interfaces for sales and data saving contracts

---
+++ src/ConsoleApp/ConsoleApp/Interfaces/ISale.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.Interfaces
+{
+    public interface ISale
+    {
+        // This is an interface definition in C#.
+        // An interface defines a contract that classes can implement.
+        // It can contain method signatures, properties, events, and indexers.
+        // Classes that implement this interface must provide implementations for its members.
+        decimal Total { get; set; } // This property defines a contract for the Total property.
+        string GetInfo(); // This method defines a contract for the GetInfo method.
+    }
+}
---
+++ src/ConsoleApp/ConsoleApp/Interfaces/ISave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// Imports basic types and utilities (e.g., Console, DateTime).
+using System.Collections.Generic;
+// Provides access to generic collections like List<T>, Dictionary<TKey, TValue>.
+using System.Linq;
+// Enables LINQ queries for collections.
+using System.Text;
+// Used for text manipulation (e.g., StringBuilder).
+using System.Threading.Tasks;
+// Supports asynchronous programming with Task and related types.
+
+
+namespace ConsoleApp.Interfaces
+{
+    public interface ISave // This is another interface definition in C#.
+    {
+        // This interface defines a contract for saving data.
+        public void Save(); // This method defines a contract for the Save method.
+    }
+}
